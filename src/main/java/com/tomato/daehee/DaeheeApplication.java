7b89d2afbf00690ded685bc71189532c261a21cd
Ginaben <iamgood147@gmail.com>
2023-06-12 23:02:53 +0900

    first commit

---
+++ src/main/java/com/tomato/daehee/DaeheeApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.daehee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomato/daehee/DaeheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.daehee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaeheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
